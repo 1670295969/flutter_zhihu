bb94589585714a1cd344d1febd0c0c70d26680c8
liuzhaofeng <1670295969@qq.com>
2018-08-14 15:09:52 +0800

    加入卡片布局

--- lib/newsItem.dart
+++ lib/newsItem.dart
@@ -4,6 +4,7 @@ import 'package:learn_zhihu_flutter/bean.dart';
 
 class NewsItemWidget extends StatefulWidget {
   final NewsItem item;
+
   NewsItemWidget(this.item);
 
   @override
@@ -16,6 +17,7 @@ class NewsItemStatus extends State<NewsItemWidget> {
   final NewsItem item;
 
   NewsItemStatus(this.item);
+
   void _itemClick(BuildContext context) {
     WebPage.jump2Web(context, item.content, item.id);
   }
@@ -26,34 +28,32 @@ class NewsItemStatus extends State<NewsItemWidget> {
         onTap: () {
           _itemClick(context);
         },
-        child: Container(
-            margin: EdgeInsets.fromLTRB(6.0, 4.0, 8.0, 4.0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          item.content,
-                          style:
-                              TextStyle(color: Colors.black87, fontSize: 14.0),
+        child: Card(
+          child: Container(
+              margin: EdgeInsets.fromLTRB(6.0, 4.0, 8.0, 4.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            item.content,
+                            style: TextStyle(
+                                color: Colors.black87, fontSize: 14.0),
+                          ),
                         ),
                       ),
-                    ),
-                    Image.network(
-                      item.url,
-                      width: 50.0,
-                      height: 50.0,
-                    ),
-                  ],
-                ),
-                Divider(
-                  color: Colors.blue,
-                  height: 1.0,
-                )
-              ],
-            )));
+                      Image.network(
+                        item.url,
+                        width: 80.0,
+                        height: 80.0,
+                      ),
+                    ],
+                  ),
+                ],
+              )),
+        ));
   }
 }
