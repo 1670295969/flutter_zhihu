1de707305a59affa8ef060aea28367e8fbf1b045
liuzhaofeng <1670295969@qq.com>
2018-08-15 10:13:45 +0800

    抽屉类型加入

--- lib/bean.dart
+++ lib/bean.dart
@@ -2,6 +2,7 @@ class NewsItem {
   final String content;
   final String url;
   final int id;
+
   NewsItem({this.content, this.url, this.id});
 
   static List<NewsItem> fromJsonWithContent(List<dynamic> json) {
@@ -15,5 +16,26 @@ class NewsItem {
     return json.map((map) {
       return NewsItem(content: map["title"], url: map["images"]);
     }).toList();
+  }
+}
+
+class ThemeItem {
+  final String iconUrl;
+  final String description;
+  final int id;
+  final String name;
+
+  ThemeItem({this.id, this.description, this.name, this.iconUrl});
+
+  static List<ThemeItem> json2List(List<dynamic> jsonArr) {
+    return jsonArr.map(json2Item).toList();
+  }
+
+  static ThemeItem json2Item(dynamic map) {
+    return ThemeItem(
+        id: map['id'],
+        description: map['description'],
+        name: map['name'],
+        iconUrl: map['thumbnail']);
   }
 }
--- lib/drawerPage.dart
+++ lib/drawerPage.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:learn_zhihu_flutter/bean.dart';
 
 //void main() {
-//  runApp(MaterialApp(
-//    home: DrawPage(),
-//  ));
+////  runApp(MaterialApp(
+////    home: DrawPage(),
+////  ));
+//
+////  ThemeItem
 //}
 
 class DrawPage extends StatefulWidget {
@@ -14,8 +20,15 @@ class DrawPage extends StatefulWidget {
 }
 
 class _DrawPageStatus extends State<DrawPage> {
+  List<ThemeItem> themeList;
+
   _reqData() {
-    setState(() {});
+//    https://news-at.zhihu.com/api/4/
+    http.get("https://news-at.zhihu.com/api/4/themes").then((resp) {
+      Map<String, dynamic> map = json.decode(resp.body);
+      themeList = ThemeItem.json2List(map['others']);
+      setState(() {});
+    });
   }
 
   @override
@@ -24,78 +37,157 @@ class _DrawPageStatus extends State<DrawPage> {
     _reqData();
   }
 
-  Widget _head() {
-    return Container(
-      color: Colors.blue,
-      height: 150.0,
-      child: Padding(
-        padding: EdgeInsets.only(left: 8.0, top: 30.0),
-        child: Column(
+  Widget _headIconWithName() {
+    return Expanded(
+      flex: 1,
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(
+            child: Text("L"),
+          ),
+          Text(
+            "Liu",
+            style: TextStyle(color: Colors.white),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _headType() {
+    return Expanded(
+        flex: 2,
+        child: Row(
           children: <Widget>[
             Expanded(
-              flex: 1,
               child: Row(
                 children: <Widget>[
-                  CircleAvatar(
-                    child: Text("L"),
+                  Icon(
+                    Icons.star,
+                    color: Colors.white,
                   ),
                   Text(
-                    "Liu",
-                    style: TextStyle(color: Colors.white),
+                    "我的收藏",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
                   )
                 ],
               ),
             ),
             Expanded(
-                flex: 2,
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.star,
-                            color: Colors.white,
-                          ),
-                          Text(
-                            "我的收藏",
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 16.0),
-                          )
-                        ],
-                      ),
-                    ),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.arrow_downward,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "离线下载",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
 
-                    Expanded(
-                      child: Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.arrow_downward,
-                            color: Colors.white,
-                          ),
-                          Text(
-                            "离线下载",
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 16.0),
-                          )
-                        ],
-                      ),
-                    )
-
-//
-                  ],
-                )),
+  Widget _head() {
+    return Container(
+      color: Colors.blue,
+      height: 150.0,
+      child: Padding(
+        padding: EdgeInsets.only(left: 15.0, top: 30.0),
+        child: Column(
+          children: <Widget>[
+            _headIconWithName(),
+            _headType(),
           ],
         ),
       ),
     );
   }
 
+  Widget _homeItem() {
+    return GestureDetector(
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+        child: Container(
+          child: Padding(
+            padding: EdgeInsets.only(left: 15.0, top: 10.0, bottom: 10.0),
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.home,
+                  color: Colors.blue,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    "首页",
+                    style: TextStyle(color: Colors.blue, fontSize: 16.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget _typeItem(ThemeItem item) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Padding(
+        padding: EdgeInsets.only(left: 15.0, top: 10.0, bottom: 10.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Text(
+                item.name,
+                style: TextStyle(fontSize: 16.0, color: Colors.black),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Icon(
+                Icons.add,
+                color: Colors.grey,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildThemeList() {
+    if (themeList == null || themeList.length == 0) {
+      return Center(
+        child: Text("暂无数据"),
+      );
+    } else {
+      return Expanded(
+        child: ListView.builder(
+            itemCount: themeList == null ? 0 : themeList.length,
+            itemBuilder: (context, index) {
+              return _typeItem(themeList[index]);
+            }),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[_head()],
+        children: <Widget>[
+          _head(),
+          _homeItem(),
+          _buildThemeList(),
+        ],
       ),
     );
   }
